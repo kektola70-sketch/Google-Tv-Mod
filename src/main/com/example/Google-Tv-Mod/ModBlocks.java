d0995df5a49a4870a7c672b903086639786a92a9
kektola70-sketch <kektola70@gmail.com>
2026-03-20 15:38:18 +0300

    Change package name and adjust screen block material
    Updated package name and modified screen block properties.

--- src/main/com/example/Google-Tv-Mod/ModBlocks.java
+++ src/main/com/example/Google-Tv-Mod/ModBlocks.java
@@ -1,19 +1,18 @@
-package com.example.googletvmod;
+package com.kektola70.googletvmod;
 
+import com.kektola70.googletvmod.block.ScreenBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import net.minecraftforge.fml.RegistryObject as RO;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.IEventBus;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, GoogleTvMod.MODID);
 
     public static final RegistryObject<Block> SCREEN_BLOCK = BLOCKS.register("screen_block",
-            () -> new ScreenBlock(Block.Properties.create(Material.MISCELLANEOUS).hardnessAndResistance(1.0f)));
+            () -> new ScreenBlock(Block.Properties.create(Material.IRON).hardnessAndResistance(1.5f)));
 
     public static void register(IEventBus bus) {
         BLOCKS.register(bus);
